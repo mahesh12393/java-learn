2bd8a7f6c9f3011602a24c385446c9d3f40f26ad
Pulla Maheswar Reddy <maheswar.reddy.pulla@sap.com>
2025-04-13 14:32:45 +0530

    my first java program

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String []args){
+        //comments look same for single line
+        /*
+        even multi line comments looks same 
+        */
+        System.out.println("Hello World from a c++ Developer");
+
+        //also adding a print variable.
+        int var = 100;
+        System.out.println("The value of var is: " + var);
+    }
+}
